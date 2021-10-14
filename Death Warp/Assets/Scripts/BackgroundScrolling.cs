08cedc91c42495b8a8d6fb8acdf15da8587f43b0
Henry Pan <legendkiller999@hotmail.com>
2021-10-14 00:21:12 -0400

    Added a main menu with music and background music to level 1

---
+++ Death Warp/Assets/Scripts/BackgroundScrolling.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScrolling : MonoBehaviour
+{
+    public float scrollSpeed;
+
+    private Renderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        renderer.material.mainTextureOffset = new Vector2(Time.time * scrollSpeed, 0);
+    }
+
+    private void MoveBG()
+    {
+        renderer.material.mainTextureOffset = new Vector2(renderer.material.mainTextureOffset.x + 0.05f, 0);
+    }
+}
